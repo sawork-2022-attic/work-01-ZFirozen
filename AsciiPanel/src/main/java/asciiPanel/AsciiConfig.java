3146b806209e8cf38012ce9f1dd8b312f24d2944
ZFirozen <zfirozen@smail.nju.edu.cn>
2022-03-06 16:39:50 +0800

    move config to Main.java

--- AsciiPanel/src/main/java/asciiPanel/AsciiConfig.java
+++ AsciiPanel/src/main/java/asciiPanel/AsciiConfig.java
@@ -1,18 +1,18 @@
-package asciiPanel;
+// package asciiPanel;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class AsciiConfig {
+// @Configuration
+// public class AsciiConfig {
 
-    @Bean
-    public AsciiFont asciiFont() {
-        return new AsciiFont("cp437_8x8.png", 8, 8);
-    }
+//     @Bean
+//     public AsciiFont asciiFont() {
+//         return new AsciiFont("cp437_8x8.png", 8, 8);
+//     }
 
-    @Bean
-    public AsciiPanel asciiPanel() {
-        return new AsciiPanel();
-    }
-}
+//     @Bean
+//     public AsciiPanel asciiPanel() {
+//         return new AsciiPanel();
+//     }
+// }
--- AsciiPanel/src/main/java/asciiPanel/Main.java
+++ AsciiPanel/src/main/java/asciiPanel/Main.java
@@ -1,8 +1,22 @@
 package asciiPanel;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class Main {
+
+    @Bean
+    public AsciiFont asciiFont() {
+        return new AsciiFont("cp437_8x8.png", 8, 8);
+    }
+
+    @Bean
+    public AsciiPanel asciiPanel() {
+        return new AsciiPanel();
+    }
+    
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
         AsciiPanel asciiPanel = context.getBean(AsciiPanel.class);
